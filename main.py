9ed67a760ac32790b8f3e57dafb7baed9d22b377
Seungjun Park(박승준) <77137245+seungjun-Park@users.noreply.github.com>
2022-12-26 17:26:16 +0900

    argparse example file

---
+++ main.py
@@ -0,0 +1,27 @@
+import argparse
+
+from omegaconf import OmegaConf
+from pytorch_lightning.trainer import Trainer
+
+def get_parser(**parser_kwargs):
+
+    arg_parser = argparse.ArgumentParser(**parser_kwargs)
+    arg_parser.add_argument(
+        "-b",
+        "--base",
+        nargs="*",
+        metavar="base_config.yaml",
+        default=list()
+    )
+
+    return arg_parser
+
+if __name__ == "__main__":
+    p = get_parser()
+    p = Trainer.add_argparse_args(p)
+    opt, unknown = p.parse_known_args()
+    config = [OmegaConf.load(cfg) for cfg in opt.base]
+    cli = OmegaConf.from_dotlist(unknown)
+    config = OmegaConf.merge(*config, cli)
+
+    print(type(config.diffusion.params.linear_start))
