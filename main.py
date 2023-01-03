bc24d92cce07569fc61325ab2f7698da711e34fa
Seungjun Park(박승준) <77137245+seungjun-Park@users.noreply.github.com>
2023-01-03 17:06:18 +0900

    testing autoencoder training code.

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import argparse
 from omegaconf import OmegaConf
 from pytorch_lightning.trainer import Trainer
 
-import importlib
+from util import get_module
+
+import matplotlib.pyplot as plt
 
 def get_parser(**parser_kwargs):
 
@@ -19,16 +21,15 @@ def get_parser(**parser_kwargs):
     return arg_parser
 
 if __name__ == "__main__":
-    p = get_parser()
-    arg = p.parse_args()
-    p = Trainer.add_argparse_args(p)
-    opt, unknown = p.parse_known_args()
-    config = [OmegaConf.load(cfg) for cfg in arg.base]
+    parsers = get_parser()
+    parsers = Trainer.add_argparse_args(parsers)
+
+    opt, unknown = parsers.parse_known_args()
+
+    configs = [OmegaConf.load(cfg) for cfg in opt.base]
     cli = OmegaConf.from_dotlist(unknown)
-    config = OmegaConf.merge(*config, cli)
+    config = OmegaConf.merge(*configs, cli)
+    lightning_config = config.pop("lightning", OmegaConf.create())
+    trainer_config = lightning_config.get("trainer", OmegaConf.create())
 
-    module_name, model_name = config.compress.model.rsplit(".", 1)
 
-    model = getattr(importlib.import_module(module_name, package=None), model_name)(config.compress)
-
-    print(model)
