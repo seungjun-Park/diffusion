69256be1d5d2b948f50f03dd3d36e61a4322fae9
Seungjun Park(박승준) <77137245+seungjun-Park@users.noreply.github.com>
2023-01-03 17:29:26 +0900

    modifying training step.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import argparse
 from omegaconf import OmegaConf
 from pytorch_lightning.trainer import Trainer
 
-from util import get_module
+from util import get_module, instantiate_from_config
 
 import matplotlib.pyplot as plt
 
@@ -20,6 +20,9 @@ def get_parser(**parser_kwargs):
 
     return arg_parser
 
+def Test(*args, **kwargs):
+    return
+
 if __name__ == "__main__":
     parsers = get_parser()
     parsers = Trainer.add_argparse_args(parsers)
@@ -31,5 +34,18 @@ if __name__ == "__main__":
     config = OmegaConf.merge(*configs, cli)
     lightning_config = config.pop("lightning", OmegaConf.create())
     trainer_config = lightning_config.get("trainer", OmegaConf.create())
+    trainer_config['accelerator'] = 'ddp'
 
+    Test(*lightning_config, **config)
 
+    trainer_opt = argparse.Namespace(**trainer_config)
+
+    model = instantiate_from_config(config.model)
+
+    trainer_kwargs = dict()
+
+    data = get_module(config.data)
+    data.prepare_data()
+    data.setup()
+
+    t =
